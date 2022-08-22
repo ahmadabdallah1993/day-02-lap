05fa4dd3df86c61ba33ea3e64d52eb24371e8562
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-22 18:41:31 +0300

    a

---
+++ my-app/src/Fotter.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+
+class Fotter extends React.Component {
+
+    render() {
+        return(
+            <div>
+          <p>Ahmad yaghi</p>
+            </div>
+        )
+    }
+
+}
+
+export default Fotter;
+
+
+
+
+
+
+
