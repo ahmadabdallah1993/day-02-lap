624aa383dd509f2013b99ff3225e15fbb6dccc78
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-22 19:54:44 +0300

    b

--- my-app/src/HornedBeast.js
+++ my-app/src/HornedBeast.js
@@ -1,6 +1,10 @@
 
 
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 
@@ -8,11 +12,24 @@ class HornedBeast extends React.Component {
 
     render() {
         return (
+
+
             <div>
-           <h2>{this.props.title}</h2>
+           {/* <h2>{this.props.title}</h2>
            <p>{this.props.id}</p>
            <img src={this.props.img} alt='img'></img> 
-           <p>{this.props.description}</p>
+           <p>{this.props.description}</p> */}
+
+           <Card style={{ width: '18rem' }}>
+           <Card.Img variant="top" src={this.props.img} />
+           <Card.Body>
+           <Card.Title>{this.props.title}</Card.Title>
+           <Card.Text>
+          number of pets: 0
+          </Card.Text>
+          {/* <Button variant="primary">Go somewhere</Button> */}
+          </Card.Body>
+           </Card>
         </div>
         )
 
